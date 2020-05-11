e0f325a69dc60f8d8117d4efe4e90440b7c75bca
alan741 <alan_ike_99@hotmail.com>
2020-05-11 11:27:54 -0500

    Version para choi

--- views/inicio.view.php
+++ views/inicio.view.php
@@ -5,72 +5,69 @@
         <title>Sign in</title>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-
-    </head>
+        <link rel="stylesheet" href="css/InicioSesion.css">
+    
+        <script src="https://kit.fontawesome.com/8b850b0e85.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <title>Inicio de Sesión</title>
+        <link rel="stylesheet" href="css/estilosRegistro.css">
+     </head>
     <body>
-        
-        <!--Barra de navegación-->
-        <nav class="navbar-default  navbar-static-top">   
-            <div class="container">
-                <div class="navbar-header text-center">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"  aria-expanded="true" aria-controls="navbar">    
-                        <span class="sr-only"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <br>
-                    <a class="navbar-brand" href="#">¡Bienvenido!</a> 
-                </div> 
-                <div id="navbar" class="navbar-collapse collapse">
-                    <form class="form-inline" name="login" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
-                        <br>
-                        <input id="nick" class="form-control mr-sm-4" type="text" placeholder="Nickname" name="Usuario">
-                        <input id="pass" class="form-control mr-sm-4" type="password" placeholder="Password" name="Password">
-                        <button onclick="login.submit()" class="btn btn-outline-success my-2 my-sm-0" type="submit">
-                            <span class="glyphicon glyphicon-log-in"></span>
-                            Sign in
-                        </button>
-                        <?php if(!empty($errores)):?>
-                            <div class="error">
-                              <ul>
-                                 <?php
-                                    echo $errores;
-                                ?>
-                             </ul>
-                            </div>
-                        <?php endif; ?>
-                    </form>
-                </div>
-            </div>      
-        </nav>
-        <br>
-       
-        <!--JumboTron-->
-        <div class="container text-center">
-            <div class="jumbotron">
-                <div class="row">
-                    <div class="col-md-6">
-                        <br>
-                        <br>
-                        <br>
-                        <h1>TastyWings</h1>
-                        <p>Boneless, alitas y más...</p>
-                    </div>
-                    <div class="col-md-6-6">
-                        <img src="https://scontent.fntr6-1.fna.fbcdn.net/v/t1.0-9/59549783_610885669423238_5947629230706655232_n.jpg?_nc_cat=108&_nc_sid=dd9801&_nc_ohc=Fgu8uitKNekAX88mk1T&_nc_ht=scontent.fntr6-1.fna&oh=d5154304bd62f63ad54f415e9c683e90&oe=5EA8F578" class="img-circle img-responsive">
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-        <script src="Botones.js"></script>
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  
+<div class="sidenav">
+  <div class="login-main-text text-center">
+    
+          <img src="css/Usuarios.png" class="rounded text-center">
+   
+      <div class="row text-center">
+        <h2 class = "display-3 font-italic text-center">&nbsp; &nbsp; &nbsp; Sign in</h2>
+        <p class = "font-italic text-center">¿Has olvidado tu contraseña? ¡Contacta a tu administrador! </p>
+      </div>      
+  </div >
+</div>
+<div class="main">
+  
+  <div class="jumbotron">
+    <h1 class = "display-3  font-italic text-center" >T A S T Y &nbsp; &nbsp; W I N G S  </h1>   
+  </div> 
+
+  <form class="jumbotron" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+    <h5 class = "display-4 font-italic " >Ingresa tu información </h1>
+    <br>
+
+    <div class="row">
+
+      <div class="col-md-2"> 
+      </div> 
+
+      <div class="form-group col-md-8">
+        <label class ="font-italic">Ingresa tu usuario</label>
+        <input type="text" class="form-control inputa" placeholder="nickname" id="usuario" name="Usuario">
+      </div>
+
+    </div>   
+
+    <div class="row">
+      <div class="col-md-2"> 
+      </div>     
+
+      <div class="form-group col-md-8">
+        <label class ="font-italic">Ingresa tu contraseña</label>
+        <input type="password" class="form-control inputa" placeholder="Password" id="password" name="Password">
+      </div>
+
+    </div>
+    <br>
+    <button type="submit" id="boton" class="btn btn-outline-success" onclick="login.submit()" >Login</button>
+    
+    <?php if(!empty($errores)):?>
+     <div class="error text-center">
+          <?php
+            echo $errores;
+          ?>
+      </div>
+      <?php endif; ?>
+  </form>
+</div>
     </body>
 </html>
